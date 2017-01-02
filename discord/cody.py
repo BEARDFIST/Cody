290392bc37f3eb8795b2984eebe1710e6c10fbd6
lmn <leon.haland@gmail.com>
2017-01-02 23:54:37 +0100

    Add files via upload

--- discord/cody.py
+++ discord/cody.py
@@ -1,33 +1,83 @@
-#python3
 #-*- encoding: utf-8 -*-
-
+import discord
+import traceback
+import os
 import sys
 
-if sys.version_info.major.__str__() != "3":
-    print("ERROR: Python 3 is necessary")
-    exit(1)
+from functions import commands
+from functions import configparser
 
-import discord
-import random
-from configparser import parsejson
+botfile     = __file__
+botpath     = sys.path[0] #os.sep.join(__file__.split(os.sep)[:-1])
+client      = discord.Client()
+conf        = configparser.config()
+bot         = conf['botname']
+account     = conf['account']
+password    = conf['password']
+server_name = conf['server']['name']
+server_id   = conf['server']['id']
+home_name   = conf['home_channel']['name']
+home_id     = conf['home_channel']['id']
+server      = discord.Server(name=server_name, id=server_id)
+home        = discord.Channel(name=home_name, id=home_id, server=server)
+prev_resp   = ''
+reload_msg  = "reloading..."
 
-config = parsejson()
-email = config["account"]["email"]
-password = config["account"]["password"]
+# extend conf dict with bot file context
+conf['botfile'] = botfile
+conf['botpath'] = botpath
 
-choices = [":fire:",":skier:"]
+switch = {
+    '!apache'   : commands.apache,
+    '!reload'   : commands.reload,
+    '!diag'     : commands.diag,
+    '!username' : commands.username,
+    '!roles'    : commands.roles,
+    '!channel'  : commands.channel,
+    '!server'   : commands.server,
+    '!error'    : commands.error,
+    '!help'     : commands.help,
+    '!web'      : commands.web,
+    '!currency' : commands.currency,
+    '!kpm'      : commands.kpm,
+    '!image'    : commands.image,
+    '!gif'      : commands.gif
+}
 
-client = discord.Client()
+# raise Exception("Deliberate error in main")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+    mc = message.content.lower()
 
-    if botname in message.content.lower() or botname in message.content.lower():
-        response = random.choice(choices)
-        await client.send_message(message.channel, response)
+    if " " in mc:
+        trigger, msg = mc.split(" ", 1)
+    else:
+        trigger = mc
+
+    try:
+        # we do not want the bot to reply to itself
+        if message.author == client.user:
+            return
+        
+        # triggers
+        elif trigger in switch.keys():
+            response = switch[trigger](trigger, message, conf)
+            await client.send_message(message.channel, response)
+
+        # conversation
+        elif mc.endswith(' cody'):
+            global prev_resp
+            response, prev_resp = commands.hi(message, conf, prev_resp)
+            await client.send_message(message.channel, response)
+
+    except SystemExit:
+        await client.send_message(message.channel, '*'+reload_msg+'*')
+        exit(1)
+
+    except:
+        err = '''```python\n{}```'''.format(traceback.format_exc())
+        await client.send_message(home, err)
 
 try:
     @client.event
@@ -37,7 +87,15 @@ try:
         print(client.user.id)
         print('------')
 
-    client.run(email, password)
+        async for message in client.logs_from(home, limit=50):
+            if reload_msg in message.content:
+                if message.author == client.user:
+                    await client.send_message(home,"*reloaded successfully*")
+                    await client.delete_message(message)
+                    break
+        
+
+    client.run(account, password)
 except KeyboardInterrupt:
     print('quit')
     exit()
